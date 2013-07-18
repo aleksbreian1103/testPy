c9a0f9ba8db5ad355d623e4e87c2385c3d954a95
Aleksander Hansen <aleksbreian@gmail.com>
2013-07-17 20:42:38 -0400

    New commit

--- Learning_Python/Learning_Python_test.py
+++ Learning_Python/Learning_Python_test.py
@@ -132,6 +132,79 @@ print( (managers|engineers) - (managers^engineers) )
 print( True + 1 )
  
 
+# Chapter 9 - Tuples and Files
+
+print((1, 2) + (3, 4))
+print( (1, 2) *4 )
+
+T = (1, 2, 3, 4)
+print(T[0], T[1:3]) 
+print(T[1:3])
+
+T = ('cc', 'aa', 'dd', 'bb')
+tmp = list(T)
+tmp.sort()
+print(tmp)
+T = tuple(tmp)
+print(T)
+print(sorted(T))
+
+# List comprehensions on tuples:
+T = (1, 2, 3, 4, 5)
+L = [x+20 for x in T]
+print(L)
+
+T = (1,2, 3, 2, 4, 2)
+print( T.index(2,2))
+
+print( T.count(2) )
+
+T = (1, [2, 3], 4)
+# cannot change tuple itself
+#T[1] = 'spam'
+
+# but we can change mutables inside a tuple
+T[1][0] = 'spam'
+print(T)
+
+# tuple as records
+bob = ('Bob', 40.5, ['dev', 'mgr'])
+print(bob)
+
+print(bob[0], bob[2])
+
+bob = dict(name = 'Bob', age = 40.5, jobs = ['dev', 'mgr'])
+print(bob)
+print(bob['name'], bob['jobs'])
+
+#converting to a tuple
+print(tuple(bob.values()))
+print(tuple(bob.items()))
+print(list(bob.items()))
+
+# access tuple by both key and position
+from collections import namedtuple
+Rec = namedtuple('Rec', ['name', 'age', 'jobs'])
+print(Rec)
+bob = Rec('Bob', age = 40.5, jobs = ['dev', 'mgr'])
+print(bob)
+# Access by position
+print(bob[0], bob[2])
+# Access by 'key'/attribute
+print(bob.name, bob.jobs)
+
+# Coverting to a dictionary supports key-based behavior when needed:
+O = bob._asdict()
+print(O)
+print(O['name'], O['jobs'])
+# Tuple assignment
+name, age, jobs = bob
+print(name, jobs)
+# Iteration context
+for x in bob: print(x)
+
+
+
 
 
 
